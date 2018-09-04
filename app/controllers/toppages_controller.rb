d2e33e03f9dc45aebd8c4de5eb8872a316025797
EC2 Default User <ec2-user@ip-172-31-29-84.us-east-2.compute.internal>
2018-09-04 12:50:10 +0000

    init

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,5 @@
 class ToppagesController < ApplicationController
   def index
+    @users = User.all.page(params[:page])
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,14 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( lib/bootstrap/dist/css/bootstrap.min )
+Rails.application.config.assets.precompile += %w( lib/animate.css/animate )
+Rails.application.config.assets.precompile += %w( lib/components-font-awesome/css/font-awesome.min )
+Rails.application.config.assets.precompile += %w( lib/et-line-font/et-line-font )
+Rails.application.config.assets.precompile += %w( lib/flexslider/flexslider )
+Rails.application.config.assets.precompile += %w( lib/owl.carousel/dist/assets/owl.carousel.min )
+Rails.application.config.assets.precompile += %w( lib/owl.carousel/dist/assets/owl.theme.default.min )
+Rails.application.config.assets.precompile += %w( lib/magnific-popup/dist/magnific-popup )
+Rails.application.config.assets.precompile += %w( lib/simple-text-rotator/simpletextrotator )
+Rails.application.config.assets.precompile += %w( css/style )
+Rails.application.config.assets.precompile += %w( css/colors/default )
